723cea9a610175cc1068db4d9293859754ab9db8
ttothewiggy <76453825+ttothewiggy@users.noreply.github.com>
2022-08-11 13:49:50 +1200

    Added counter to announce winner

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
+const winnerDisplay = document.getElementById("winner");
 
 let userChoice;
 let computerChoice;
 let result;
+let winner;
+
+let playerCounter = 0;
+let computerCounter = 0;
 
 //function for functionality of the buttons
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
@@ -15,8 +20,21 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
     getResult()
+    game()
+  
 }))
 
+//function that counts the winner
+function game(){
+if (playerCounter == 5){
+    winner = "Player"
+    winnerDisplay.innerHTML = winner; 
+}
+else if (computerCounter == 5){
+    winner = "Computer"
+    winnerDisplay.innerHTML = winner;
+}
+}
 //function that determines the computers choice
 
 function generateComputerChoice(){
@@ -43,22 +61,29 @@ function getResult() {
     }
     if (computerChoice === "rock" && userChoice === "paper"){
         result = "You win"
+        playerCounter++;
     }
     if (computerChoice === "rock" && userChoice === "scissors"){
         result = "You lost"
+        computerCounter++;
     }
     if (computerChoice === "paper" && userChoice === "scissors"){
         result = "You win"
+        playerCounter++;
     }
     if (computerChoice === "paper" && userChoice === "rock"){
         result = "You lost"
+        computerCounter++;
     }
     if (computerChoice === "scissors" && userChoice === "rock"){
         result = "You win"
+        playerCounter++;
     }
     if (computerChoice === "scissors" && userChoice === "paper"){
         result = "You lost"
+        computerCounter++;
     }
     resultDisplay.innerHTML = result
 }
+
 
